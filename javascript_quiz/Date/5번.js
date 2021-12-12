a24dfcad67404fe55c3842309f3f54fa58dc221e
ywc8851 <ywc8851@naver.com>
2021-12-12 18:50:39 +0900

    Delete javascript_quiz/Date directory

--- javascript_quiz/Date/5번.js
+++
@@ -1,9 +0,0 @@
-function getLastDayOfMonth(year, month) {
-  const day = new Date(year, month + 1, 0).getDay();
-  console.log(day);
-}
-// 2021년 1월 말일은 일요일
-getLastDayOfMonth(2021, 0); // => 0
-
-// 2021년 12월 말일은 금요일
-getLastDayOfMonth(2021, 11); // => 5
