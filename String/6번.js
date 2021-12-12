4e1f0dcb955bbb7c656b6732c8dd50333dca1cdd
ywc8851 <ywc8851@naver.com>
2021-12-12 18:49:37 +0900

    Delete String directory

--- String/6번.js
+++
@@ -1,6 +0,0 @@
-const toggleCase = (str) =>
-  str.replace(/[a-zA-Z]/g, (char) =>
-    char === char.toLowerCase() ? char.toUpperCase() : char.toLowerCase()
-  );
-
-console.log(toggleCase("StuDY")); // sTUdy
