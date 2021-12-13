8cf789159cbbc5aaacb7a73ea4cb4c93b10a8b94
ywc8851 <ywc8851@naver.com>
2021-12-13 11:30:51 +0900

    Update 4번.js

--- String/4번.js
+++ String/4번.js
@@ -3,8 +3,7 @@ function isPalindrome(str) {
   const checkpalindromeStr = palindromeStr
     .split("")
     .reverse()
-    .join()
-    .replace(/[,]/g, "");
+    .join("");   
   return palindromeStr === checkpalindromeStr;
 }
 console.log(isPalindrome("A man, a plan, a canal: Panama")); // => true
